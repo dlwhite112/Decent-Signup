d9bdcc125208a5ab3b3d36e2b057d1ce64f37d0f
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-06-10 23:57:20 -0500

    add img to Landing & Sign In - stopping point

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,30 +3,38 @@ import "./style.css";
 import facebook from "../../assets/Facebook_black.png";
 import twitter from "../../assets/Twitter_black.png";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import Docimg2 from "../../assets/doctor-img-1.jpg"
 
 export default function Landing() {
   return (
     <Router>
       <div className="container d-flex flex-column justify-content-center align-items-center">
         <h1 className="heading">Decent Healthcare for Tech Freelancers</h1>
-        <button
-          type="button"
-          className="btn btn-outline-danger mb-4 fw-bold fs-1"
-        >
-          Sign Up
-        </button>
-        <p className="fw-light">
-          Already have an account?
-          <span>
-            <Link to="/sign-in">Sign In</Link>
-          </span>
-        </p>
-        <button
-          type="button"
-          className="fw-bold btn btn-outline-danger mt-5 telegram mb-5"
-        >
-          JOIN OUR TELEGRAM
-        </button>
+        <div className="row mx-auto justify-content-center mb-3">
+            <div className="col-4 d-flex flex-column justify-content-center">
+                <button
+                  type="button"
+                  className="btn btn-outline-danger mb-4 fw-bold fs-1"
+                >
+                  Sign Up
+                </button>
+                <p className="fw-light">
+                  Already have an account?
+                  <span>
+                    <Link to="/sign-in">Sign In</Link>
+                  </span>
+                </p>
+                <button
+                  type="button"
+                  className="fw-bold btn btn-outline-danger mt-5 telegram mb-5"
+                >
+                  JOIN OUR TELEGRAM
+                </button>
+            </div>
+            <div className="col-7">
+            <img src={Docimg2} class="img-fluid" alt="doctor-consultation-img"></img>
+            </div>
+        </div>
         <div className="d-flex flex-row justify-content-center links fw-light">
           <a href="http://">ABOUT US</a> <span>|</span>{" "}
           <a href="http://">NEWS</a> <span>|</span>
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import Docimg from "../../assets/doctor-img-2.jpg"
 
 export default function SignIn() {
   return (
@@ -8,43 +9,49 @@ export default function SignIn() {
         <div className="container">
           <div className="row d-flex flex-column justify-content-start">
             <h1 className="heading">Sign In</h1>
-            <form className="col-4">
-              <div className="mb-4">
-                <label for="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email Address"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                ></input>
-              </div>
-              <div className="mb-4">
-                <label for="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                ></input>
-                <div id="passwordHelp" className="form-text">
-                  Can't remember?
-                  <span>
-                      <Link to="/sign-in">Let's reset it</Link>
-                  </span>
+            <div className="row">
+                <form className="col-4">
+                  <div className="mb-4">
+                    <label for="exampleInputEmail1" className="form-label">
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email Address"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
+                  <div className="mb-4">
+                    <label for="exampleInputPassword1" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                    ></input>
+                    <div id="passwordHelp" className="form-text">
+                      Can't remember?
+                      <span>
+                          <Link to="/sign-in">Let's reset it</Link>
+                      </span>
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    className="fw-bold btn btn-outline-danger mt-2 telegram mb-5 btn-sm"
+                  >
+                    LET ME IN!
+                  </button>
+                </form>
+                <div className="col-1"></div>
+                <div className="col-6">
+                <img src={Docimg} class="img-fluid" alt="doctor-consultation-img"></img>
                 </div>
-              </div>
-              <button
-                type="button"
-                className="fw-bold btn btn-outline-danger mt-2 telegram mb-5 btn-sm"
-              >
-                LET ME IN!
-              </button>
-            </form>
+            </div>
           </div>
         </div>
     </Router>
