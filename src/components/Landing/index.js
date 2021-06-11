e29d5c22d3eef58213f3f564d7db9212d27921ff
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-06-10 23:28:46 -0500

    add SignIn layout

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,43 +2,46 @@ import React from "react";
 import "./style.css";
 import facebook from "../../assets/Facebook_black.png";
 import twitter from "../../assets/Twitter_black.png";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function Landing() {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center">
-      <h1 className="heading">Decent Healthcare for Tech Freelancers</h1>
-      <button
-        type="button"
-        className="btn btn-outline-danger mb-4 fw-bold fs-1"
-      >
-        Sign Up
-      </button>
-      <p className="fw-light">
-        Already have an account?
-        <span>
-          <a href="http://">Sign In</a>
-        </span>
-      </p>
-      <button
-        type="button"
-        className=" fw-bold btn btn-outline-danger mt-5 telegram mb-5"
-      >
-        JOIN OUR TELEGRAM
-      </button>
-      <div className="d-flex flex-row justify-content-center links fw-light">
-        <a href="http://">ABOUT US</a> <span>|</span> <a href="http://">NEWS</a>{" "}
-        <span>|</span>
-        <a href="http://">JOBS</a> <span>|</span>{" "}
-        <a href="http://">CONTACT US</a>
+    <Router>
+      <div className="container d-flex flex-column justify-content-center align-items-center">
+        <h1 className="heading">Decent Healthcare for Tech Freelancers</h1>
+        <button
+          type="button"
+          className="btn btn-outline-danger mb-4 fw-bold fs-1"
+        >
+          Sign Up
+        </button>
+        <p className="fw-light">
+          Already have an account?
+          <span>
+            <Link to="/sign-in">Sign In</Link>
+          </span>
+        </p>
+        <button
+          type="button"
+          className="fw-bold btn btn-outline-danger mt-5 telegram mb-5"
+        >
+          JOIN OUR TELEGRAM
+        </button>
+        <div className="d-flex flex-row justify-content-center links fw-light">
+          <a href="http://">ABOUT US</a> <span>|</span>{" "}
+          <a href="http://">NEWS</a> <span>|</span>
+          <a href="http://">JOBS</a> <span>|</span>{" "}
+          <a href="http://">CONTACT US</a>
+        </div>
+        <div className="d-flex flex-row justify-content-center m-3">
+          <a href="https://twitter.com/decent">
+            <img className="social-link" src={twitter} alt="twitter-link" />
+          </a>
+          <a href="https://www.facebook.com/DecentHealth/">
+            <img className="social-link" src={facebook} alt="facebook-link" />
+          </a>
+        </div>
       </div>
-      <div className="d-flex flex-row justify-content-center m-3">
-        <a href="http://">
-          <img className="social-link" src={twitter} alt="twitter-link" />
-        </a>
-        <a href="http://">
-          <img className="social-link" src={facebook} alt="facebook-link" />
-        </a>
-      </div>
-    </div>
+    </Router>
   );
 }
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function SignIn() {
-    return (
-        <div>
-            <h1>Sign In</h1>
+  return (
+    <Router>
+        <div className="container">
+          <div className="row d-flex flex-column justify-content-start">
+            <h1 className="heading">Sign In</h1>
+            <form className="col-4">
+              <div className="mb-4">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email Address"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label for="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                ></input>
+                <div id="passwordHelp" className="form-text">
+                  Can't remember?
+                  <span>
+                      <Link to="/sign-in">Let's reset it</Link>
+                  </span>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="fw-bold btn btn-outline-danger mt-2 telegram mb-5 btn-sm"
+              >
+                LET ME IN!
+              </button>
+            </form>
+          </div>
         </div>
-    )
+    </Router>
+  );
 }
